5311d643fb32f4050a2378fa5e5f6867ac8cfe7f
James Arnold <jamesnogra@gmail.com>
2019-10-03 21:19:25 +0800

    First push.

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello world\n');
+});
+
+app.get('/sum', (req, res) => {
+    var x = parseInt(req.query.x);
+    var y = parseInt(req.query.y);
+    res.send((x+y).toString());
+  });
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
